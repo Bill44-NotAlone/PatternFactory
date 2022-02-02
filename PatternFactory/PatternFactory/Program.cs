dacd5dec91e1f2d1f459d3cbd387022941926b3c
DemonEyes12 <anemnonov2@gmail.com>
2022-02-02 11:13:43 +0300

    Паттерн фабрика. Версия 0.1

---
+++ PatternFactory/PatternFactory/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PatternFactoryLib;
+
+namespace PatternFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StorePizza store = new StorePizza();
+            Pizza pizza = store.orderPizza("Сырная");
+            Console.WriteLine(pizza.Display());
+            Console.WriteLine("===");
+
+            pizza = store.orderPizza("Веганская");
+            Console.WriteLine(pizza.Display());
+            Console.WriteLine("===");
+
+            pizza = store.orderPizza("Морская");
+            Console.WriteLine(pizza.Display());
+            Console.WriteLine("===");
+
+            pizza = store.orderPizza("Пеперони");
+            Console.WriteLine(pizza.Display());
+            Console.WriteLine("===");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PatternFactory/PatternFactoryLib/ClamPizza.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternFactoryLib
+{
+    public class ClamPizza : Pizza
+    {
+        public ClamPizza()
+        {
+            description = "Морская пицца:";
+        }
+
+        public override string Cut()
+        {
+            return "Пицца не нарезана.";
+        }
+    }
+}
